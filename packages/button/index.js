ed8f80c27b09ed3a9e0d007fe5da99b5590d79c1
Louis <louiscd@126.com>
2020-09-27 18:30:35 +0800

    [feature]-[新增babel-plugin-component 插件实现分包加载]

--- packages/button/index.js
+++ packages/button/index.js
@@ -1,6 +1,7 @@
-import Button from './src/button.vue'
+import Button from './src/button.vue';
 // 为组件提供 install 安装方法，供按需引入
 Button.install = function (Vue) {
-  Vue.component(Button.name, Button)
-}
-export default Button
+  console.log('== > 按需加载 Button < ==');
+  Vue.component(Button.name, Button);
+};
+export default Button;
